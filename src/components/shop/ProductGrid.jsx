dd6f13c8b7b50b1810e118f523df5d2609ca6665
Rydge <96926831+rydge-rivard@users.noreply.github.com>
2023-12-21 10:10:13 -0500

    Add prod list state and effect to fetch on init.

---
+++ src/components/shop/ProductGrid.jsx
@@ -0,0 +1,5 @@
+export default function ProductGrid({ productList }) {
+  console.log(productList);
+
+  return <ul>{/* <li>{productList}</li> */}</ul>;
+}
--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,25 +1,30 @@
+import { useEffect, useState } from "react";
 import Nav from "../NavBar";
+import ProductGrid from "./ProductGrid";
 
 export default function Shop() {
-  let productList = [];
+  const [productList, setProductList] = useState([]);
 
   async function getProducts() {
     try {
       const response = await fetch(`https://fakestoreapi.com/products?limit=5`);
       const products = await response.json();
-      productList = products;
+      setProductList(products);
     } catch (err) {
       console.log(err);
     }
-    console.log(productList);
   }
 
-  getProducts();
+  useEffect(() => {
+    getProducts();
+  }, []);
 
   return (
     <div className="cart">
       <Nav></Nav>
-      <div>SHOP</div>
+      <div className="product-grid">
+        <ProductGrid productList={productList} />
+      </div>
     </div>
   );
 }
