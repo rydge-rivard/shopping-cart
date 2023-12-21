7b44aae92013c1438a51a45c662ac2b5ce2d98e1
Rydge <96926831+rydge-rivard@users.noreply.github.com>
2023-12-21 10:27:06 -0500

    Add product grid component to render products.

--- src/components/shop/ProductGrid.jsx
+++ src/components/shop/ProductGrid.jsx
@@ -1,5 +1,20 @@
+import PropTypes from "prop-types";
+
 export default function ProductGrid({ productList }) {
   console.log(productList);
 
-  return <ul>{/* <li>{productList}</li> */}</ul>;
+  const listItems = productList.map((product) => (
+    <li key={product.id}>
+      <img src={product.image} />
+      <p>{product.title}</p>
+      <p>${product.price}</p>
+      <button>ADD TO CART</button>
+    </li>
+  ));
+
+  return <ul>{listItems}</ul>;
 }
+
+ProductGrid.propTypes = {
+  productList: PropTypes.array.isRequired,
+};
